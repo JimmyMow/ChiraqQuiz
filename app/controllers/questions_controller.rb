17e74076a21277e4882f051baa9e61760b4d6fa6
Jack Mallers <jimmymowschess@gmail.com>
2014-01-23 12:20:52 -0600

    fixing a few bugs

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -63,7 +63,7 @@ class QuestionsController < ApplicationController
 
   def qua
     Qua.create(user_id: current_user.id, answer_id: params[:id], score: params[:score], question_id: params[:question_id])
-    if params[:question_id] == '30'
+    if params[:question_id] == '10'
       redirect_to user_url(current_user.id)
     else
       redirect_to question_url(params[:question_id].to_i + 1 )
